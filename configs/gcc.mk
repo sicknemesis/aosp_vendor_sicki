3bd4b6fd4df73afaa3756ed28e4bd049d96b5a00
Sicknemesis <hulluhalonen10@gmail.com>
2015-05-24 16:52:31 +0300

    Update GCC info

--- configs/gcc.mk
+++ configs/gcc.mk
@@ -10,8 +10,9 @@ GCC_AND := $(shell $(GCC_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
 # Find strings in version info
 GCC_AND_NAME := $(filter (UBERTC%) (GCC%) (SaberMod%) (SaberNaro%) (LinaroMod%) (Linaro%),$(GCC_AND))
 GCC_AND_NUMBER := $(filter 4.8 4.8.% 4.9 4.9.% 4.10 4.10.%,$(GCC_AND))
-GCC_AND_VERSION := $(GCC_AND_NAME)-($(GCC_AND_NUMBER))
+GCC_AND_DATE := $(filter 20140% 20141% 20150% 20151%,$(GCC_AND))
+GCC_AND_STATUS := $(filter (release) (prerelease) (experimental),$(GCC_AND))
+GCC_AND_VERSION := $(GCC_AND_NAME)-($(GCC_AND_NUMBER))-$(GCC_AND_DATE)-$(GCC_AND_STATUS)
 #Adds values to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.gcc.android=$(GCC_AND_VERSION)
-
